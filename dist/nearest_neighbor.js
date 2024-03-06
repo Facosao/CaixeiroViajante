0fe9404440a86716ad247b712aa2a4df9affd875
Facosao <leonardosalgado02@hotmail.com>
2024-03-06 17:59:33 -0300

    .

--- dist/nearest_neighbor.js
+++ dist/nearest_neighbor.js
@@ -1,11 +1,13 @@
 import { Draw } from "./draw.js";
 export function nearest_neighbor(points) {
+    let path = [];
+    path.push(0);
     for (let i = 0; i < points.length; i++) {
+        Draw.clearScreen();
+        Draw.circle(points[i], "red");
+        Draw.points(points);
         let smallest_distance = null;
         let smallest_distance_index = null;
-        Draw.clearScreen();
-        Draw.points(points);
-        Draw.circle(points[i], "red");
         for (let j = i + 1; j < i + points.length; j++) {
             const distance = points[i].distanceTo(points[j % points.length]);
             if ((smallest_distance === null) || (distance < smallest_distance)) {
